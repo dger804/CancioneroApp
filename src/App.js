bf3e862d58d9adfba585add68433082123735365
dger804 <diego804@gmail.com>
2021-02-02 21:02:13 -0500

    Se implementa barra de navegación y mockeado de referencias por crear rutas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import {Navigation} from './components/Navigation'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <Navigation/>
       </header>
     </div>
   );
