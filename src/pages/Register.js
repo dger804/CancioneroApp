9c9f11969549adda150f0a46b069268c3b24254f
dger804 <diego804@gmail.com>
2021-02-04 14:35:52 -0500

    Se implementa los elementos para el formulario de registro con los input y el botón de registro

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,9 +1,50 @@
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 import '../App.css';
 
 export function Register(){
   return(
     <div className="main">
-      Registrarse
+      <Container fluid>        
+        <Row className="justify-content-md-center">
+          <Col sm="auto">
+            <Form className="registerform">
+              <h1>Registrarse</h1>
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label>Email</Form.Label>
+                <Form.Control className="mr-sm-2"
+                  type="email"
+                  placeholder="Ingrese email"
+                  name="email"
+                  required
+                />                
+              </Form.Group>
+              <Form.Group controlId="formBasicPassword">
+                <Form.Label>Contraseña</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Contraseña"
+                  name="password"
+                  required
+                />
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Confirmar contraseña</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Confirm Password"
+                  name="confirmPassword"
+                  required
+                />
+              </Form.Group>
+              <Button variant="primary" type="submit"
+                className="form-control"
+                >
+                Registrarse
+              </Button>
+            </Form> 
+          </Col>
+        </Row>
+      </Container>
     </div>  
   );
 }
