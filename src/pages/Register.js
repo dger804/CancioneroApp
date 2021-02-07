cb970bb2b07e4052d1835b063fa24f8d256d943d
dger804 <diego804@gmail.com>
2021-02-07 18:54:55 -0500

    Se crean campos en el formulario para el registro de un usuario nuevo, se implementa el reducer de registro, se genera token para la sesión y se realizan validación sobre la contraseña

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,13 +1,61 @@
-import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import { Container, Row, Col, Form, Button, Alert } from 'react-bootstrap';
+import { useHistory } from 'react-router-dom';
 import '../App.css';
+import { regUser } from '../store/registerReducer'
+import { useDispatch, useSelector } from 'react-redux'
+import { useState, useEffect } from 'react';
 
 export function Register(){
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  const { loading, register } = useSelector(
+    ( { registerReducer: { loading, register } } ) => {
+      return { loading, register }
+    }
+  )
+
+  useEffect(() => {
+    const token = sessionStorage.getItem('token')
+    if (token) {
+      history.push('/')
+      return(
+        <Alert variant="succes">Bienvenido</Alert>
+      )
+    }
+  }, [register])
+
+  const [regForm, setRegForm] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+    userType: "Cliente"
+  })
+
+  const handleChange = (e) => {
+    setRegForm({
+      ...regForm,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    const { email, password, confirmPassword } = regForm
+    
+    if(password === confirmPassword){
+      dispatch(regUser(email, password))
+    }else{
+      <Alert variant="danger">Contraseñas no coinciden</Alert>
+    }
+  }
+  const { email, password, confirmPassword } = regForm
   return(
     <div className="main">
       <Container fluid>        
         <Row className="justify-content-md-center">
           <Col sm="auto">
-            <Form className="registerform">
+            <Form onSubmit={handleRegister} className="registerform">
               <h1>Registrarse</h1>
               <Form.Group controlId="formBasicEmail">
                 <Form.Label>Email</Form.Label>
@@ -15,6 +63,8 @@ export function Register(){
                   type="email"
                   placeholder="Ingrese email"
                   name="email"
+                  onChange={handleChange}
+                  value={email}
                   required
                 />                
               </Form.Group>
@@ -24,6 +74,8 @@ export function Register(){
                   type="password"
                   placeholder="Contraseña"
                   name="password"
+                  onChange={handleChange}
+                  value={password}
                   required
                 />
               </Form.Group>
@@ -33,11 +85,14 @@ export function Register(){
                   type="password"
                   placeholder="Confirmar contraseña"
                   name="confirmPassword"
+                  onChange={handleChange}
+                  value={confirmPassword}
                   required
                 />
               </Form.Group>
               <Button variant="primary" type="submit"
                 className="form-control"
+                disabled={loading}
                 >
                 Registrarse
               </Button>
