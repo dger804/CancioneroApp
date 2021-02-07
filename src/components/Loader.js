9d1b9f77af41719d0ee3db16cee90c10a3da2556
dger804 <diego804@gmail.com>
2021-02-07 18:45:53 -0500

    Se crea componente para la carga del estado de los elementos que vienen del back

---
+++ src/components/Loader.js
@@ -0,0 +1,9 @@
+import { Container } from 'react-bootstrap'
+
+export function Loader(){
+  return(
+    <div className="loader">
+      <span className="sr-only">Loading...</span>
+    </div>
+  )
+}
