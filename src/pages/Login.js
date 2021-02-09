f1e1086b7dfc3a48928b56d1e0f0a8d5958ffe29
dger804 <diego804@gmail.com>
2021-02-08 21:22:45 -0500

    Se crea el formulario de ingreso

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,43 @@
 import '../App.css';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap'
 
 export function Login(){
   return(
     <div className="main">
-      Ingreso
+      <Container>
+        <Row className="justify-content-md-center">
+          <Col md="auto">
+            <h1>Ingresar</h1>
+            <Form.Group controlid="FormBasicEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Escriba email"
+                name="email"
+
+                required
+              />
+            </Form.Group>
+            <Form.Group controlid="formBasicEmail">
+              <Form.Label>Contraseña</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Contraseña"
+                name="password"
+                
+                required
+              />
+            </Form.Group>
+            <Button
+              className="form-control"
+              variant="primary"
+              type="submit"
+              >
+              Ingresar
+            </Button>
+          </Col>
+        </Row>
+      </Container>
     </div>  
   );
 }
