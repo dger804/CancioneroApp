2aba05267f830c3d47cee683611bad0de6898716
dger804 <diego804@gmail.com>
2021-02-09 01:22:51 -0500

    Se implementa el reducer en el formulario de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,40 +1,101 @@
 import '../App.css';
 import { Container, Row, Col, Form, Button } from 'react-bootstrap'
+import { useHistory } from "react-router-dom"
+import { useState, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import Swal from 'sweetalert2'
+import { userLogin } from '../store/loginReducer'
 
 export function Login(){
+  const history = useHistory();
+  const dispatch = useDispatch();
+  
+  const { login, loading } = useSelector(
+    ({ loginReducer: { login, loading } }) =>{
+      return { login, loading }
+    }
+  )
+/*
+  useEffect(() => {
+    if(errorLog){
+      Swal.fire({
+        title: 'Error!',
+        text: `${ errorLog.message }`,
+        icon: 'error',
+        confirmButtonText: 'Intentar de nuevo'
+      })
+      dispatch( cleanuperror( errorLog ) )
+    }
+  },[errorLog])*/
+
+  useEffect(() => {
+    const token = sessionStorage.getItem('token')
+    if(token){
+      Swal.fire({
+        title: 'Ingreso Exitoso!',
+        text: 'Bienvenido',
+        icon: 'success',
+        showConfirmButton: false,
+        timer: 1500
+      })
+      history.push('/')
+    }
+  },[login])
+  const [ loginForm, setLoginForm ] = useState({
+    email: "",
+    userType: "user",
+    password: "",
+  })
+  const handleChange = e => {
+    setLoginForm({
+      ...loginForm,
+      [e.target.name]: e.target.value
+    })
+  }
+  const handleLogin = async e =>{
+    e.preventDefault();
+    const { email, password } = loginForm;
+    dispatch(userLogin(email, password));
+  }
+  const { email, password } = loginForm
   return(
     <div className="main">
       <Container>
         <Row className="justify-content-md-center">
           <Col md="auto">
-            <h1>Ingresar</h1>
-            <Form.Group controlid="FormBasicEmail">
-              <Form.Label>Email</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="Escriba email"
-                name="email"
-
-                required
-              />
-            </Form.Group>
-            <Form.Group controlid="formBasicEmail">
-              <Form.Label>Contraseña</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Contraseña"
-                name="password"
-                
-                required
-              />
-            </Form.Group>
-            <Button
-              className="form-control"
-              variant="primary"
-              type="submit"
-              >
-              Ingresar
-            </Button>
+            <Form className="loginForm" onSubmit={handleLogin}>
+              <h1>Ingresar</h1>
+              <Form.Group controlid="FormBasicEmail">
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Escriba email"
+                  name="email"
+                  value={email}
+                  required
+                  onChange={handleChange}
+                />
+              </Form.Group>
+              <Form.Group controlid="formBasicEmail">
+                <Form.Label>Contraseña</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Contraseña"
+                  name="password"
+                  value={password}
+                  required
+                  onChange={handleChange}
+                />
+              </Form.Group>
+              <Button
+                className="form-control"
+                variant="primary"
+                type="submit"
+                disabled={loading}
+                >
+                Ingresar
+              </Button>
+            </Form>
           </Col>
         </Row>
       </Container>
